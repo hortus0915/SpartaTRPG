93b5d0fe20ef4388b4f2099e61010494f11988a6
hortus0915 <86999699+hortus0915@users.noreply.github.com>
2025-09-10 16:01:37 +0900

    플레이어 수치 수정

--- SpartaTRPG/Blue/Card/CardDB.cpp
+++ SpartaTRPG/Blue/Card/CardDB.cpp
@@ -70,6 +70,7 @@ namespace CardDB
 		AddCard(Card(303, "보급", HEAL, 0, 15.0f, -20));
 		AddCard(Card(304, "회복", HEAL, 0, 50.0f, 50));
 
+
 		// ===== 공격 (401~) =====
 		AddCard(Card(401, "찌르기", ATTACK, (DIR_L | DIR_C | DIR_R), 1.5f, 30));
 		AddCard(Card(402, "회전 베기", ATTACK, (DIR_UL | DIR_U | DIR_UR | DIR_L | DIR_R | DIR_DL | DIR_D | DIR_DR), 1.3f, 20));
@@ -80,10 +81,10 @@ namespace CardDB
 		AddCard(Card(407, "대각선 베기", ATTACK, DIAGONAL4, 1.1f, 40));
 		AddCard(Card(408, "원형 일격", ATTACK, PERIMETER8, 1.0f, 50));
 
-		AddCard(Card(409, "부채꼴 찌르기(상)", ATTACK, CONE_U_3, 1.15f, 30));
-		AddCard(Card(410, "부채꼴 베기(하)", ATTACK, CONE_D_3, 1.15f, 30));
-		AddCard(Card(411, "부채꼴 베기(좌)", ATTACK, CONE_L_3, 1.15f, 30));
-		AddCard(Card(412, "부채꼴 베기(우)", ATTACK, CONE_R_3, 1.15f, 30));
+		AddCard(Card(409, "부채꼴 찌르기(상)", ATTACK, T_U, 1.15f, 30));
+		AddCard(Card(410, "부채꼴 베기(하)", ATTACK, T_D, 1.15f, 30));
+		AddCard(Card(411, "부채꼴 베기(좌)", ATTACK, T_L, 1.15f, 30));
+		AddCard(Card(412, "부채꼴 베기(우)", ATTACK, T_R, 1.15f, 30));
 
 		AddCard(Card(413, "충격파", ATTACK, ALL9, 0.65f, 50));
 
